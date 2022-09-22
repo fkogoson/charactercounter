ee685887c78b94f09539a26c2a4d95edf7c31250
Festus Kogo <fkogoson@gmail.com>
2022-09-22 15:58:42 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -2,6 +2,4 @@ my_text = "Translation of simple business requirements into technical specificat
           "deployment decisions can be very difficult The protection mechanism itself is subject to " \
           "attack Protectors have to be right all the time. Attackers only have to be right once."
 
-
-my_numbers = "1234567890"
 print(len(my_text))
