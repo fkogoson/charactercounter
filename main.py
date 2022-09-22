8698c33e85acbbcecbff27b2ac31d3ee1d60a478
Festus Kogo <fkogoson@gmail.com>
2022-09-22 15:48:11 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+my_text = "Translation of simple business requirements into technical specifications and " \
+          "deployment decisions can be very difficult The protection mechanism itself is subject to " \
+          "attack Protectors have to be right all the time. Attackers only have to be right once."
+
+
+my_numbers = "1234567890"
+print(len(my_text))
